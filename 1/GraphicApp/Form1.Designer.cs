d856a0ccc12ab582b2fa37fd690db75f99109790
Saireeex <153263630+Saireeex@users.noreply.github.com>
2024-10-30 14:32:55 +0300

    Add files via upload

---
+++ 1/GraphicApp/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace GraphicApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lineButton = new System.Windows.Forms.Button();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.rectangleButton = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lineButton
+            // 
+            this.lineButton.Location = new System.Drawing.Point(12, 12);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(75, 23);
+            this.lineButton.TabIndex = 0;
+            this.lineButton.Text = "Линия";
+            this.lineButton.UseVisualStyleBackColor = true;
+            // 
+            // circleButton
+            // 
+            this.circleButton.Location = new System.Drawing.Point(93, 12);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(75, 23);
+            this.circleButton.TabIndex = 1;
+            this.circleButton.Text = "Круг";
+            this.circleButton.UseVisualStyleBackColor = true;
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.Location = new System.Drawing.Point(174, 12);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(75, 23);
+            this.rectangleButton.TabIndex = 2;
+            this.rectangleButton.Text = "Квадрат";
+            this.rectangleButton.UseVisualStyleBackColor = true;
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 44);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(776, 394);
+            this.panel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.circleButton);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.rectangleButton);
+            this.Controls.Add(this.lineButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lineButton;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.Button rectangleButton;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ 1/GraphicApp/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphicApp
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private bool drawing;
+        private string currentShape = "Line";
+        private List<Shape> shapes = new List<Shape>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lineButton.Click += (s, e) => currentShape = "Line";
+            circleButton.Click += (s, e) => currentShape = "Circle";
+            rectangleButton.Click += (s, e) => currentShape = "Rectangle";
+
+            var buttonPanel = new FlowLayoutPanel { Dock = DockStyle.Top, Height = 30 };
+            buttonPanel.Controls.Add(lineButton);
+            buttonPanel.Controls.Add(circleButton);
+            buttonPanel.Controls.Add(rectangleButton);
+
+            this.Controls.Add(buttonPanel);
+
+            panel.MouseDown += (s, e) =>
+            {
+                drawing = true;
+                startPoint = e.Location;
+            };
+
+            panel.MouseMove += (s, e) =>
+            {
+                if (drawing)
+                {
+                    endPoint = e.Location;
+                    panel.Invalidate();
+                }
+            };
+
+            panel.MouseUp += (s, e) =>
+            {
+                drawing = false;
+                endPoint = e.Location;
+                shapes.Add(new Shape(currentShape, startPoint, endPoint));
+                panel.Invalidate();
+            };
+
+            panel.Paint += (s, e) =>
+            {
+                foreach (var shape in shapes)
+                {
+                    shape.Draw(e.Graphics);
+                }
+
+                if (drawing)
+                {
+                    DrawShape(e.Graphics);
+                }
+            };
+        }
+
+        private void DrawShape(Graphics g)
+        {
+            Pen pen = Pens.Black;
+
+            switch (currentShape)
+            {
+                case "Line":
+                    g.DrawLine(pen, startPoint, endPoint);
+                    break;
+                case "Circle":
+                    g.DrawEllipse(pen, GetRectangle(startPoint, endPoint));
+                    break;
+                case "Rectangle":
+                    g.DrawRectangle(pen, GetRectangle(startPoint, endPoint));
+                    break;
+            }
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            return new Rectangle(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y),
+                                 Math.Abs(p1.X - p2.X), Math.Abs(p1.Y - p2.Y));
+        }
+    }
+
+    public class Shape
+    {
+        public string ShapeType { get; }
+        public Point StartPoint { get; }
+        public Point EndPoint { get; }
+
+        public Shape(string shapeType, Point startPoint, Point endPoint)
+        {
+            ShapeType = shapeType;
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = Pens.Black;
+            switch (ShapeType)
+            {
+                case "Line":
+                    g.DrawLine(pen, StartPoint, EndPoint);
+                    break;
+                case "Circle":
+                    g.DrawEllipse(pen, GetRectangle(StartPoint, EndPoint));
+                    break;
+                case "Rectangle":
+                    g.DrawRectangle(pen, GetRectangle(StartPoint, EndPoint));
+                    break;
+            }
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            return new Rectangle(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y),
+                                 Math.Abs(p1.X - p2.X), Math.Abs(p1.Y - p2.Y));
+        }
+    }
+}
